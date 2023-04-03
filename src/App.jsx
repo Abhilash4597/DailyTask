28eaf1870e80f847cf12fe98fbe7f1bc280479b5
Abhilash4597 <abhilash4597@gmail.com>
2023-04-04 01:54:01 +0530

    Css and code changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,74 @@
 import React, { useState } from 'react';
 import './App.css';
-import TodoInput from './Components/TodoInput';
-import Todolist from './Components/Todolist';
+// import TodoInput from './Components/TodoInput';
+// import Todolist from './Components/Todolist';
 
+// function App() {
+//   const [listTodo, setListTodo] = useState([]);
+
+//   let addList = inputText => {
+//     if (inputText !== '') {
+//       setListTodo([...listTodo, inputText])
+//     };
+//   };
+
+//   const deleteListItem = key => {
+//     let newListTodo = [...listTodo];
+//     newListTodo.splice(key, 1);
+//     setListTodo([...newListTodo]);
+//   };
+//   return (
+//     <div>
+//       <div>
+//         <TodoInput addList={addList} />
+//         <h1>TODO</h1>
+//         <hr />
+//         {listTodo.map((listItem, i) => {
+//           return (
+//             <Todolist
+//               key={i}
+//               index={i}
+//               item={listItem}
+//               deleteItem={deleteListItem}
+//             />
+//           );
+//         })}
+//       </div>
+//     </div>
+//   );
+// }
 function App() {
-  const [listTodo, setListTodo] = useState([]);
+  const [item, setItem] = useState('');
+  const [listItem, setListItem] = useState([]);
 
-  let addList = inputText => {
-    if (inputText !== '') {
-      setListTodo([...listTodo, inputText])
-    };
-  };
+  function handleChange(e) {
+    let inputValue = e.target.value;
+    setItem(inputValue);
+  }
 
-  const deleteListItem = key => {
-    let newListTodo = [...listTodo];
-    newListTodo.splice(key, 1);
-    setListTodo([...newListTodo]);
-  };
+  function handleClick() {
+    setListItem(preValue => {
+      return [...preValue, item];
+    });
+    setItem('')
+  }
   return (
-    <div>
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input type="text" onChange={handleChange} value={item} />
+        <button onClick={handleClick}>
+          <span>Add</span>
+        </button>
+      </div>
       <div>
-        <TodoInput addList={addList} />
-        <h1>TODO</h1>
-        <hr />
-        {listTodo.map((listItem, i) => {
-          return (
-            <Todolist
-              key={i}
-              index={i}
-              item={listItem}
-              deleteItem={deleteListItem}
-            />
-          );
-        })}
+        <ul>
+          {listItem.map(item => {
+            return <li>{item}</li>;
+          })}
+        </ul>
       </div>
     </div>
   );
