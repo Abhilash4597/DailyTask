3009adf6b5e8342b7020c46690f2bcf79875950c
Abhilash4597 <abhilash4597@gmail.com>
2023-04-04 16:45:49 +0530

    style added and add functionality working

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-// import TodoInput from './Components/TodoInput';
-// import Todolist from './Components/Todolist';
 
-// function App() {
-//   const [listTodo, setListTodo] = useState([]);
-
-//   let addList = inputText => {
-//     if (inputText !== '') {
-//       setListTodo([...listTodo, inputText])
-//     };
-//   };
-
-//   const deleteListItem = key => {
-//     let newListTodo = [...listTodo];
-//     newListTodo.splice(key, 1);
-//     setListTodo([...newListTodo]);
-//   };
-//   return (
-//     <div>
-//       <div>
-//         <TodoInput addList={addList} />
-//         <h1>TODO</h1>
-//         <hr />
-//         {listTodo.map((listItem, i) => {
-//           return (
-//             <Todolist
-//               key={i}
-//               index={i}
-//               item={listItem}
-//               deleteItem={deleteListItem}
-//             />
-//           );
-//         })}
-//       </div>
-//     </div>
-//   );
-// }
 function App() {
   const [item, setItem] = useState('');
   const [listItem, setListItem] = useState([]);
@@ -50,7 +14,7 @@ function App() {
     setListItem(preValue => {
       return [...preValue, item];
     });
-    setItem('')
+    setItem('');
   }
   return (
     <div className="container">
@@ -64,11 +28,19 @@ function App() {
         </button>
       </div>
       <div>
-        <ul>
-          {listItem.map(item => {
-            return <li>{item}</li>;
-          })}
-        </ul>
+        {listItem.map((item, i) => {
+          return (
+            <ul key={i}>
+              <li className='list'>
+                {item}
+                <span>
+                  <i className="fa-solid fa-trash-can icon-delete"></i>
+                  <i className="fa-solid fa-pen-to-square"></i>
+                </span>
+              </li>
+            </ul>
+          );
+        })}
       </div>
     </div>
   );
